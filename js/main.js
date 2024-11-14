8a67adf6132f2b98ff01dad20f364eca63f25cec
jorgeazapata <jorgeandresz8910@hotmail.com>
2024-11-14 05:03:05 -0500

    version 2.0 completa

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('calculation-form');
+    const taraCanastaSelect = document.getElementById('taracanasta');
+
+    taraCanastaSelect.addEventListener('change', function() {
+        if (taraCanastaSelect.value === '1.8') {
+            taraCanastaSelect.classList.remove('bg-green');
+            taraCanastaSelect.classList.add('bg-red');
+        } else if (taraCanastaSelect.value === '2.0') {
+            taraCanastaSelect.classList.remove('bg-red');
+            taraCanastaSelect.classList.add('bg-green');
+        }
+    });
+
     form.addEventListener('submit', function(event) {
         event.preventDefault(); // Evita el envío del formulario
 
         // Obtén los valores de los inputs como decimales
         const numCanastas = parseFloat(document.getElementById('numcanastas').value);
-        const taraCanasta = parseFloat(document.getElementById('taracanasta').value);
+        const taraCanasta = parseFloat(taraCanastaSelect.value);
         const pesoBruto = parseFloat(document.getElementById('pesobruto').value);
 
         // Realiza el cálculo
         const resultado = pesoBruto - (numCanastas * taraCanasta);
 
         // Muestra el resultado
-        document.getElementById('resultado').textContent = ` ${resultado.toFixed(2)}`;
+        document.getElementById('resultado').textContent = `${resultado.toFixed(2)} kl.`;
     });
 });
